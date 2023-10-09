7fa960ed416d182a1539ff98a4b9292785f2dd5e
Vlad <vlados80008@gmail.com>
2023-10-09 21:13:52 +0300

    .

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -133,6 +133,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void CheckForLegitimacy()
     {
+        // игрок не может находиться в воздухе больше 3 сек.
         if (_playerGroundCheck.IsGround)
             _airTimeoutDelta = 0;
         else
@@ -144,6 +145,7 @@ public class PlayerMovement : MonoBehaviour
             _airTimeoutDelta = 0;
         }
 
+        // игрок не может быть ниже чем чекпоинт - _dead
         if (transform.position.y < _vectorPoint.y - _dead && !_playerGroundCheck.IsGround)
         {
             Dead();
